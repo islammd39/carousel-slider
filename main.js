fe6cce69ed74efc0df954045d4b32050dfb77697
islammd39e <islammdrahat308@gmail.comm>
2022-09-18 14:45:57 +0600

    class of object

--- main.js
+++ main.js
@@ -10,15 +10,32 @@ const images = [
     'images/slide-9.jpg',
 ]
 let index = 0
- 
+
 const sliderImage = document.getElementById('carousel-img')
-setInterval(() =>{
-    if(index === images.length){
+setInterval(() => {
+    if (index === images.length) {
         index = 0;
     }
     const slide = images[index]
-    sliderImage.setAttribute('src',slide)
-    console.log(sliderImage);
+    sliderImage.setAttribute('src', slide)
+    // console.log(sliderImage);
     index++;
-    
-},3000)
+
+}, 3000)
+
+// this is just practice =====>>>>>>
+
+class Data {
+    constructor(name, clas, section, roll, age, bestFriend) {
+        this.name = name;
+        this.clas = clas;
+        this.section = section;
+        this.roll = roll;
+        this.age = age;
+        this.bestFriend = bestFriend;
+    }
+}
+
+const oneStudent = new Data('Abbas', 'Four', 'A', 1, 9, 'Rohit')
+const twoStudent = new Data('adnan', 'Four', 'B', 2, 10, 'Rofic')
+console.log(twoStudent);
