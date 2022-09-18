b708ac5a1c733d1145ad973eb241e881a0991de0
islammd39e <islammdrahat308@gmail.comm>
2022-09-18 14:25:02 +0600

    finel carosel upload

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const images = [
     'images/slide-1.jpg',
     'images/slide-2.jpg',
@@ -10,14 +9,16 @@ const images = [
     'images/slide-8.jpg',
     'images/slide-9.jpg',
 ]
-let indexOfImages = 0;
-const img = document.getElementById('carousel-img')
-setInterval(() => {
-    if(indexOfImages === images.length){
-        indexOfImages = 0;
+let index = 0
+ 
+const sliderImage = document.getElementById('carousel-img')
+setInterval(() =>{
+    if(index === images.length){
+        index = 0;
     }
-    const slide = images[indexOfImages]
-    img.setAttribute('src', slide)
-    console.log(slide);
-    indexOfImages ++;
-}, 2000);
+    const slide = images[index]
+    sliderImage.setAttribute('src',slide)
+    console.log(sliderImage);
+    index++;
+    
+},3000)
